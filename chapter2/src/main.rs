78f69993c15d227225f3f2122f9ce26d75a41b38
reppets <all.you.need.is.word@gmail.com>
2021-07-27 02:35:29 +0900

    Add Chapter 2

---
+++ chapter2/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let image_width = 256;
+    let image_height = 256;
+
+    println!("P3");
+    println!("{} {}", image_width, image_height);
+    println!("255");
+
+    for y in (0..image_height).rev() {
+        eprint!("\rScanlines remaining: {}", y);
+        for x in 0..image_width {
+            let r = x as f64 / (image_width - 1) as f64;
+            let g = y as f64 / (image_height - 1) as f64;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+    eprintln!("\nDone.");
+}
