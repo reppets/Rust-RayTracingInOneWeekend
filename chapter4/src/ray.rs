bdf7f0b240a69de31f5ab264a69c8873cfc15d36
reppets <all.you.need.is.word@gmail.com>
2021-08-04 10:34:16 +0900

    Add Chapter 4

---
+++ chapter4/src/ray.rs
@@ -0,0 +1,12 @@
+use crate::vec3::{Point, Vec3};
+
+pub struct Ray{
+    pub origin: Point,
+    pub direction: Vec3,
+}
+
+impl Ray {
+    pub fn at(self, t: f64) -> Point {
+        self.origin + t * self.direction
+    }
+}
